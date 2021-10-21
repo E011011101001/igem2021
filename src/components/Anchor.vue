0fd8aa1bb305486902ae838a7cef1ed05f5d0f28
Weisi Wang <wangweisi.night12138@bytedance.com>
2021-10-21 20:02:53 +0800

    anchor components

---
+++ src/components/Anchor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+const AnchorProps = Vue.extend({
+  props: {
+    ele: HTMLElement
+  }
+})
+
+@Component
+export default class Anchor extends AnchorProps {
+  static id = 'Anchor'
+
+  mounted (): void {
+    const ele = this.ele
+    if (ele) {
+      const anchors = ele.querySelectorAll(
+        'h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]'
+      )
+      console.log(anchors)
+    }
+  }
+}
+</script>
